f5b013580da484f2d94359ba1cb1753d5e551ca1
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-05 20:50:10 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,18 @@
 
 module Memorable
+  module ClassMethods
 
-  def reset_all
-    self.all.clear
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
 
-  def count
-    self.all.count
-  end
 
+  module InstanceMethods
+
+  end
 end
