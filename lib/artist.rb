d20528bbcb71d0c734459185775b8eca635c034e
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-05 20:50:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,6 @@
 require 'pry'
 
 class Artist
-  extend Memorable
-  include Paramable
-
   attr_accessor :name
   attr_reader :songs
 
@@ -22,13 +19,13 @@ class Artist
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-  #
-  # def self.count
-  #   self.all.count
-  # end
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
 
   def add_song(song)
     @songs << song
@@ -39,7 +36,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+  def to_param
+    name.downcase.gsub(' ', '-')
+  end
 end
