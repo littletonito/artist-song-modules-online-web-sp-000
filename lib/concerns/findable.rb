761800e92a1d2270cb26ea600af5962e486a5800
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-05 20:55:07 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,9 @@
 
 module Findable
+  module InstanceMethods
 
-  def find_by_name(name)
-    self.all.find { |j| j.name == name }
+    def find_by_name(name)
+      self.all.find { |j| j.name == name }
+    end
   end
 end
