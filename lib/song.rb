4f2894aef3df01d393ce50df3547acbb262439ee
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-04-05 20:51:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 class Song
-  extend Memorable
-  include Paramable
-
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
